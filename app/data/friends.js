7967ec032a2425a2b6b7ce632cb44242da72c526
Jadan Morrow <jadanmorrow@outlookcom>
2020-04-27 18:16:37 -0600

    adding files

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// JSON obejct holding the data of all the friends
+var friends = [
+  {
+    name: 'Tony Stark',
+    photo: 'https://i.redd.it/49o9y71u5j331.jpg',
+    scores: [4, 3, 3, 3, 4, 3, 2, 1, 5, 3],
+  },
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static('app/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+  //Jarvis
+  console.log('Welcome back, sir. Your app is listening on port: ', PORT);
+});
