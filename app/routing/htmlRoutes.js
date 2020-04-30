e3169fc8319654b7d2c16afa698342c3a22ad477
Jadan Morrow <jadanmorrow@outlookcom>
2020-04-29 18:38:03 -0600

    htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,9 +4,9 @@ module.exports = function (app) {
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
-
+  //Jarvis
   app.get('*', function (req, res) {
-    console.log('here');
+    console.log('Hello, sir');
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 };
